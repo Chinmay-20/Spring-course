466bc3abfe419fd749efcc34908c87efdd049db3
Chinmay Kulkarni <59417006+Chinmay-20@users.noreply.github.com>
2022-10-07 21:51:21 -0400

    Add files via upload

---
+++ 7-Oct/CI/Person.java
@@ -0,0 +1,23 @@
+package com.springcore.ci;
+
+import java.util.List;
+
+public class Person {
+	private String name;
+	private int personid;
+	private Certi certi;
+	private List<String> list;
+	public Person(String name, int personid, Certi certi,List<String> list) {
+		this.name = name;
+		this.personid = personid;
+		this.certi=certi;
+		this.list=list;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [name=" + this.name + ", personid=" + this.personid +", certi= "+this.certi.getName() + ", list = "+this.list + "]";
+	}
+	
+	
+}
